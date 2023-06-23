2b1444c0622328b654296af213a37d3714d505c6
RodolfoPlaquin <135379012+RodolfoPlaquin@users.noreply.github.com>
2023-06-23 15:44:35 -0300

    Valida datos ingresados por el usuario

--- tareas-resueltas/clase-5/Tarea2/script.js
+++ tareas-resueltas/clase-5/Tarea2/script.js
@@ -8,7 +8,7 @@ $botonMostrar.onclick = function(){
     let $edadUsuario = document.querySelector('#edad-usuario');
     let $mostrarDatos = document.querySelector('#mostrar-datos');
     let $tituloSaludo = document.querySelector('#titulo-saludo');
-    
+
     $mostrarDatos.innerHTML =   "Primer nombre: " + $primerNombreUsuario.value + "<br>" +
                                 "Segundo nombre: " + $segundoNombreUsuario.value + "<br>" +
                                 "Apellido/s: " + $ApellidosUsuario.value + "<br>" +
@@ -17,4 +17,48 @@ $botonMostrar.onclick = function(){
     $tituloSaludo.innerText = `Bienvenido, ${$primerNombreUsuario.value}!`;
 
     return false;
+}
+
+function validarNombres ( nombre ){
+    const regexNombre = /^[a-zA-ZÑñÁáÉéÍíÓóÚúÜü\s]+$/;
+    if(nombre.value !== ""){
+        if(!regexNombre.test(nombre)){
+            return "Solo debe contener letras";
+        }
+        const cantidadLetrasNombre = nombre.split("").length;
+        if(cantidadLetrasNombre < 3){
+            return "Cantidad de letras insuficientes";
+        }
+    }
+    if(nombre.value === ""){
+        return "El campo está vacío";
+    }
+    return true;
+}
+
+function validarApellidos ( apellido ){
+    const regexApellido = /^([A-Za-zÑñÁáÉéÍíÓóÚú]+['\-]{0,1}[A-Za-zÑñÁáÉéÍíÓóÚú]+)(\s+([A-Za-zÑñÁáÉéÍíÓóÚú]+['\-]{0,1}[A-Za-zÑñÁáÉéÍíÓóÚú]+))*$/;
+    if(apellido.value !== ""){
+        if(!regexApellido.test(apellido)){
+            return "Solo debe contener letras";
+        }
+        const cantidadLetrasApellido = apellido.split("").length;
+        if(cantidadLetrasApellido < 3){
+            return "Cantidad de letras insuficientes";
+        }
+    }
+    if(apellido.value === ""){
+        return "El campo está vacío";
+    }
+    return true;
+}
+
+function validarEdad ( edad ){
+    if(isNaN( edad )){
+            return "Ingresar un número válido";
+        }
+    if(apellido.value === ""){
+        return "El campo está vacío";
+    }
+    return true;
 }

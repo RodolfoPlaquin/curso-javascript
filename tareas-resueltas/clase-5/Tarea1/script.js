c454cc84ebdaf446fa0f429466e6acf2272292e9
RodolfoPlaquin <135379012+RodolfoPlaquin@users.noreply.github.com>
2023-06-23 11:45:45 -0300

    Funcionalidad de cálculo de salario mensual
    Se agrega el código en el archivo script.js para calcular el salario mensual.

---
+++ tareas-resueltas/clase-5/Tarea1/script.js
@@ -0,0 +1,17 @@
+const $botonCalcular = document.querySelector('#boton-calcular-salario');
+
+$botonCalcular.onclick = function(){
+    const $salarioAnual = document.querySelector('#salario-anual').value;
+    const MESES_EN_EL_ANIO = 12;
+    const salarioMensual =  ($salarioAnual / MESES_EN_EL_ANIO).toFixed(2);
+
+    if(!isNaN(salarioMensual)){
+        const $mostrarSalarioMensual = document.querySelector('#salario-mensual');
+        $mostrarSalarioMensual.value = salarioMensual; 
+    } else {
+        const $mostrarSalarioMensual = document.querySelector('#salario-mensual');
+        $mostrarSalarioMensual.value = ""; 
+    } 
+
+    return false;
+}

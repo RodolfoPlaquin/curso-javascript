bfd77e480cbc7c74ec924877a16811ca7d40dff8
RodolfoPlaquin <135379012+RodolfoPlaquin@users.noreply.github.com>
2023-06-27 12:23:25 -0300

    Muestra los resultados en la pantalla luego de presionar el botón calcular

--- tareas-resueltas/clase-6/tarea2/script.js
+++ tareas-resueltas/clase-6/tarea2/script.js
@@ -1,54 +1,89 @@
 const $botonAgregarFamiliar = document.querySelector('#agregar-familiar');
 const $generarFormulario = document.querySelector('#formulario-salarios');
+const $mostrarResultado = document.querySelector('#mostrar-resultado');
+
 let cantidadFamiliares = 0;
 
-function agregarFormulario( formulario ){
+function agregarFormulario(formulario) {
     formulario.innerHTML = "";
-    
-    for(let i = 1 ; i<=cantidadFamiliares ; i++){
+
+    for (let i = 1; i <= cantidadFamiliares; i++) {
         const $crearLabel = document.createElement('label');
         $crearLabel.textContent = `Salario de familiar N°${i} :`;
-        
+
         const $crearInput = document.createElement('input');
         $crearInput.id = "salarioFamiliar" + i;
         $crearInput.type = "number";
-        
+
         const $crearBotonEliminarRegistro = document.createElement('button');
         $crearBotonEliminarRegistro.textContent = "Eliminar Registro";
-        $crearBotonEliminarRegistro.addEventListener ('click' , () => eliminarRegistro($crearLabel , $crearInput, $crearBotonEliminarRegistro));
-        
+        $crearBotonEliminarRegistro.addEventListener('click', () => eliminarRegistro($crearLabel, $crearInput, $crearBotonEliminarRegistro));
+
         formulario.appendChild($crearLabel);
         formulario.appendChild($crearInput);
         formulario.appendChild($crearBotonEliminarRegistro);
     }
 }
 
-function eliminarRegistro(label , input , button){
+function eliminarRegistro(label, input, button) {
     label.remove();
     input.remove();
     button.remove();
     cantidadFamiliares--;
-    
+    $mostrarResultado.innerHTML = "";
     if (cantidadFamiliares === 0)
         $generarFormulario.innerHTML = "";
-} 
+}
 
-function botonCalcular (formulario){
-    if(cantidadFamiliares> 0){
+function botonCalcular(formulario) {
+    if (cantidadFamiliares > 0) {
         const $crearBotonCalcular = document.createElement('button');
         $crearBotonCalcular.id = "calcular-salarios";
         $crearBotonCalcular.textContent = "Calcular";
         formulario.appendChild($crearBotonCalcular);
+
+        $crearBotonCalcular.onclick = function () {
+            const salarios = arraySalarios($generarFormulario);
+            const salarioPromedio = calcularSalarioPromedio(salarios);
+            const salarioMaximo = Math.max(...salarios);
+            const salarioMinimo = Math.min(...salarios);
+            mostrarResultados(salarioPromedio, salarioMaximo, salarioMinimo);
+            return false;
+        };
     }
 }
 
-$botonAgregarFamiliar.onclick = function(){
+function arraySalarios(formulario) {
+    const arraySalarios = [];
+    for (let i = 0; i < cantidadFamiliares; i++) {
+        arraySalarios.push(Number(formulario[i].value));
+    }
+    return arraySalarios;
+}
+
+function calcularSalarioPromedio(arraySalarios) {
+    let sumadorSalarios = 0;
+    for (let i = 0; i < cantidadFamiliares; i++) {
+        sumadorSalarios += arraySalarios[i];
+    }
+    const promedio = sumadorSalarios / arraySalarios.length;
+    return promedio;
+}
+
+function mostrarResultados(promedio, max, min) {
+    $mostrarResultado.innerHTML = "El salario promedio es: " + promedio + "<br>" +
+        "El salario máximo es: " + max + "<br>" +
+        "El salario mínimo es: " + min;
+}
+
+$botonAgregarFamiliar.onclick = function () {
     cantidadFamiliares++;
     agregarFormulario($generarFormulario);
-    if(cantidadFamiliares > 0)
+    if (cantidadFamiliares > 0) {
         botonCalcular($generarFormulario);
-   
+    }
     return false;
-}
+};
+
 
 

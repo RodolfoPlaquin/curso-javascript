efb8b745d05d9fde2c47dcdd094bbd7d6d225c89
RodolfoPlaquin <135379012+RodolfoPlaquin@users.noreply.github.com>
2023-06-27 11:40:22 -0300

    Crea funcionalidad al botón de eliminar registro y agrega botón calcular

--- tareas-resueltas/clase-6/tarea2/script.js
+++ tareas-resueltas/clase-6/tarea2/script.js
@@ -1,38 +1,54 @@
 const $botonAgregarFamiliar = document.querySelector('#agregar-familiar');
+const $generarFormulario = document.querySelector('#formulario-salarios');
 let cantidadFamiliares = 0;
-$botonAgregarFamiliar.onclick = function(){
-    const $generarFormulario = document.querySelector('#calcular-salario');
-    $generarFormulario.innerHTML = "";
-    cantidadFamiliares++;
+
+function agregarFormulario( formulario ){
+    formulario.innerHTML = "";
+    
     for(let i = 1 ; i<=cantidadFamiliares ; i++){
-        const crearLabel = document.createElement('label');
-        crearLabel.id = "labelFamiliar" + i;
-        crearLabel.textContent = `Salario de familiar N°${i} :`;
+        const $crearLabel = document.createElement('label');
+        $crearLabel.textContent = `Salario de familiar N°${i} :`;
         
-        const crearInput = document.createElement('input');
-        crearInput.id = "salarioFamiliar" + i;
-        crearInput.type = "number";
+        const $crearInput = document.createElement('input');
+        $crearInput.id = "salarioFamiliar" + i;
+        $crearInput.type = "number";
         
-        const crearBotonEliminarRegistro = document.createElement('button');
-        crearBotonEliminarRegistro.id = "eliminarRegistro" + i;
-        crearBotonEliminarRegistro.textContent = "Eliminar Registro";
+        const $crearBotonEliminarRegistro = document.createElement('button');
+        $crearBotonEliminarRegistro.textContent = "Eliminar Registro";
+        $crearBotonEliminarRegistro.addEventListener ('click' , () => eliminarRegistro($crearLabel , $crearInput, $crearBotonEliminarRegistro));
         
-        $generarFormulario.appendChild(crearLabel);
-        $generarFormulario.appendChild(crearInput);
-        $generarFormulario.appendChild(crearBotonEliminarRegistro);
-        
-        
-        $generarFormulario.appendChild(crearLabel);
-        $generarFormulario.appendChild(crearInput);
-        $generarFormulario.appendChild(crearBotonEliminarRegistro);
-        
-        crearBotonEliminarRegistro.onclick = function() {
-            const parent = this.parentNode;
-            parent.removeChild(crearLabel);
-            parent.removeChild(crearInput);
-            parent.removeChild(crearBotonEliminarRegistro);
-            cantidadFamiliares--;
-        }
+        formulario.appendChild($crearLabel);
+        formulario.appendChild($crearInput);
+        formulario.appendChild($crearBotonEliminarRegistro);
     }
+}
+
+function eliminarRegistro(label , input , button){
+    label.remove();
+    input.remove();
+    button.remove();
+    cantidadFamiliares--;
+    
+    if (cantidadFamiliares === 0)
+        $generarFormulario.innerHTML = "";
+} 
+
+function botonCalcular (formulario){
+    if(cantidadFamiliares> 0){
+        const $crearBotonCalcular = document.createElement('button');
+        $crearBotonCalcular.id = "calcular-salarios";
+        $crearBotonCalcular.textContent = "Calcular";
+        formulario.appendChild($crearBotonCalcular);
+    }
+}
+
+$botonAgregarFamiliar.onclick = function(){
+    cantidadFamiliares++;
+    agregarFormulario($generarFormulario);
+    if(cantidadFamiliares > 0)
+        botonCalcular($generarFormulario);
+   
     return false;
 }
+
+

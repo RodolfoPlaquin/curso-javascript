eef18ea0c335071e3f4b11467db376b5b6a5e343
RodolfoPlaquin <135379012+RodolfoPlaquin@users.noreply.github.com>
2023-07-11 16:06:04 -0300

    Corrige ejercicios de clases anteriores

--- tareas-resueltas/clase-5/Tarea3/main.js
+++ tareas-resueltas/clase-5/Tarea3/main.js
@@ -7,52 +7,6 @@ let segundosTotales = 0;
 let minutosTotales = 0;
 let horasTotales = 0;
 
-
-function calcularSegundos (){
-    segundosTotales = 0; 
-    for(let i = 1 ; i<=$cantidadVideos ; i++){
-        const segundos= Number(document.querySelector(`#segundos${i}`).value);
-        if (segundos >= 0 && segundos <= 59) {
-            segundosTotales += segundos;
-        } else {
-           
-            alert(`Segundos inválidos en el video ${i}. Debe estar entre 0 y 59.`);
-            return;
-        }
-    }
-}
-
-function calcularMinutos (){
-    minutosTotales = 0;
-    for(let i = 1 ; i<=$cantidadVideos ; i++){
-        const minutos = Number(document.querySelector(`#minutos${i}`).value);
-        if (minutos >= 0 && minutos <= 59) {
-            minutosTotales += minutos;
-        } else {
-            alert(`Minutos inválidos en el video ${i}. Debe estar entre 0 y 59.`);
-            return;
-        }
-    }
-    
-    if(segundosTotales >= 60){
-        minutosTotales += Math.floor(segundosTotales / 60);
-        segundosTotales %= 60;
-    }
-}
-
-function calcularHoras (){
-    horasTotales = 0;
-    for(let i = 1 ; i<=$cantidadVideos ; i++){
-        const horas = Number(document.querySelector(`#horas${i}`).value);
-        horasTotales += horas;
-    }
-    
-    if(minutosTotales >= 60){
-        horasTotales += Math.floor(minutosTotales / 60);
-        minutosTotales %= 60;
-    }
-}
-
 function calcularTiempoTotal(){
     calcularSegundos();
     calcularMinutos();
